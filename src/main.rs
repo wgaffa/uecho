b285691004a3e9ae1bf17e2008b00d1ae3db0068
Patrik Maunus <subscription@skriptladan.se>
2022-06-10 16:35:12 +0200

    First draft working
    Send either hex (0x1f345) or decimal numbers to stdin and
    it will echo the corresponding unicode character or � if it can't convert it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::char::REPLACEMENT_CHARACTER;
+
 fn main() {
-    println!("Hello, world!");
+    for input in std::io::stdin().lines() {
+        if let Ok(input) = input {
+            let words = input.split_whitespace()
+                .map(|x| into_unicode(x) );
+
+            for word in words {
+                print!("{}", word);
+            }
+        }
+    }
+}
+
+fn into_unicode(input: &str) -> char {
+    let input = match input.get(0..2) {
+        Some(hex) if hex == "0x" => u32::from_str_radix(&input[2..], 16),
+        Some(_) => u32::from_str_radix(input, 10),
+        _ => panic!("Error when reading stdin"),
+    };
+
+    let input = input.unwrap_or(REPLACEMENT_CHARACTER as u32);
+    char::from_u32(input).unwrap_or(REPLACEMENT_CHARACTER)
 }
